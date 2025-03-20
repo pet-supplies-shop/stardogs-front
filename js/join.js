ad9f79775cc5a4e79ac77daa1e9771dc87c3286a
Hanyeojun <hyj78396@naver.com>
2025-03-21 04:35:01 +0900

    코드 정리

--- js/join.js
+++ js/join.js
@@ -1,21 +1,4 @@
-// // // 회원가입
-// document
-//   .getElementById("insertMemberBtn")
-//   .addEventListener("click", async () => {
-//     const name = document.getElementById("joinName").value; //입력한 이름
-//     const email = document.getElementById("joinEmail").value; //입력한 이메일
-//     const pwd = document.getElementById("joinPwd").value; //입력한 비밀번호
-
-//     const memberdata = { email, pwd, name }; //json 형태로 만듦
-
-//     const response = await axios.post(
-//       "http://localhost:8080/insertMember",
-//       memberdata
-//     );
-//     alert("회원가입 완료!");
-
-//     // console.log(memberdata);
-//   });
+//회원가입
 document
   .getElementById("insertMemberBtn")
   .addEventListener("click", async () => {
